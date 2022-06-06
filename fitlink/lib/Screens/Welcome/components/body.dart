af5a25b81509b88885fcdbc2b7d0be520ee1deb3
Couto <manuelcouto10@ua.pt>
2022-06-06 19:39:26 +0100

    first paage

---
+++ fitlink/lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,42 @@
+import 'package:fitlink/Screens/Welcome/components/background.dart';
+import 'package:fitlink/components/rounded_button.dart';
+import 'package:fitlink/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/icons/fitLogin.png",
+            height: size.height * 0.2,
+            width: size.width * 0.7,), 
+            SizedBox(height: size.height * 0.1,  // espaço entre imagem e fitlink
+            ),
+            Text(
+            "FitLink™",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
+          ),
+          RoundedButton(
+            text: "Login",
+            press: () {},
+          ),
+          RoundedButton(
+            text: "Sign in with Email",
+            color: kPrimaryLightColor,
+            textColor: Colors.black,
+            press: () {},
+          ),
+          ],
+          ),
+      ),
+    );
+  }
+}
